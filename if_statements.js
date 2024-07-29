f158ecf14aef1f2b0e9b21c02f63eaabb1e4082c
Laimonas <ldirgelas530@gmail.com>
2024-07-29 15:23:52 +0300

    first commit

---
+++ if_statements.js
@@ -0,0 +1,47 @@
+if (true) {
+    console.log('This is true');
+}
+
+if (false) {
+    console.log('This is NOT true');
+}
+
+
+
+
+const x = 10;
+const y = 5;
+
+if (x >= y) {
+console.log(`${x} is greater than ${y}`);
+
+
+};
+
+if (x === y) {
+    console.log(`${x} is equal to ${y}`);
+    
+    
+    } else {
+        console.log(`${x} is no equal to ${y}`);
+
+    };
+
+
+if (x !== y) {
+
+    var z = 20;
+    console.log(`${z} is 20`);
+}
+
+console.log(z);
+
+
+//Shorthand if
+
+
+if (x >= y) 
+    console.log(`${x} is GREATER than ${y}`),
+console.log('this is true');
+else console.log('THIS IS FALSE');
+    
